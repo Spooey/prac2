8273bcd252a2f9bd06346263ccfbd55ade935dab
Angus Buchanan <angus1252@gmail.com>
2023-09-10 23:32:18 +1000

    Added required imports for the GAN

---
+++ GAN.py
@@ -0,0 +1,18 @@
+# Part 3 GAN
+#%matplotlib inline
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+from matplotlib.animation import FuncAnimation
